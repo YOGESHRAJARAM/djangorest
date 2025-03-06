790b4934c0e7f887e9fcc3bb170e134bf8eba0ae
YogeshRajaram <yogiyogeshrajaram@gmail.com>
2025-03-06 13:50:05 +0530

    model serializer

--- watchmate/watchlist_app/api/serializer.py
+++ watchmate/watchlist_app/api/serializer.py
@@ -1,26 +1,11 @@
 from rest_framework import serializers
 from watchlist_app.models import Movie
 
-def cheack_description(value):
-      if len(value) < 2 :
-             raise serializers.ValidationError("discription is too short")
-      return value
 
-class MovieSerializer(serializers.Serializer):
-       id = serializers.IntegerField(read_only=True)
-       name = serializers.CharField()
-       description = serializers.CharField(validators=[cheack_description])
-       active = serializers.BooleanField()
-       
-       def create(self, validated_data):
-              return Movie.objects.create(**validated_data)
-       
-       def update(self, instance, validated_data):
-              instance.name=validated_data.get('name',instance.name)
-              instance.description=validated_data.get('description',instance.description)
-              instance.active=validated_data.get('active',instance.active)
-              instance.save()
-              return instance
+class MovieSerializer(serializers.ModelSerializer):
+       class Meta:
+              model = Movie
+              fields = '__all__'
        
        def validate_name(self,value):
               if len(value) < 2:
--- watchmate/watchlist_app/api/views.py
+++ watchmate/watchlist_app/api/views.py
@@ -19,19 +19,6 @@ class Movie_listAV(APIView):
             return Response(serializer.errors)
         
 
-# @api_view(['GET','POST'])
-# def movie_list(request):
-#     if request.method == 'GET':
-#         movies = Movie.objects.all()
-#         serializer = MovieSerializer(movies,many=True)
-#         return Response(serializer.data)
-#     if request.method == 'POST':
-#         serializer = MovieSerializer(data=request.data)
-#         if serializer.is_valid():
-#             serializer.save()
-#             return Response(serializer.data)
-#         else:
-#             return Response(serializer.errors)
 
 
 class Movle_DetailsAV(APIView):
@@ -66,24 +53,3 @@ class Movle_DetailsAV(APIView):
 
 
 
-
-
-# @api_view(['GET','PUT','DELETE'])
-# def movie_details(request,pk):
-#     if request.method == 'GET':
-#         movie = Movie.objects.get(pk=pk)
-#         serializer = MovieSerializer(movie)
-#         return Response(serializer.data)
-#     if request.method == 'PUT':
-#         movie = Movie.objects.get(pk=pk)
-#         serializer = MovieSerializer(movie,data=request.data)
-#         if serializer.is_valid():
-#             serializer.save()
-#             return Response(serializer.data)
-#         else:
-#             return Response(serializer.errors)
-        
-#     if request.method == 'DELETE':
-#         movie = Movie.objects.get(pk=pk)
-#         movie.delete()
-#         return Response()
