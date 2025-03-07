2250a35c128f9cc9908813779170711db6703afe
YogeshRajaram <yogiyogeshrajaram@gmail.com>
2025-03-07 10:39:00 +0530

    serializer custom field

--- watchmate/watchlist_app/api/serializer.py
+++ watchmate/watchlist_app/api/serializer.py
@@ -3,10 +3,14 @@ from watchlist_app.models import Movie
 
 
 class MovieSerializer(serializers.ModelSerializer):
+       name_length = serializers.SerializerMethodField()
        class Meta:
               model = Movie
               fields = '__all__'
        
+       def get_name_length(self,object):
+           return len(object.name)
+
        def validate_name(self,value):
               if len(value) < 2:
                      raise serializers.ValidationError("Name too Short")
