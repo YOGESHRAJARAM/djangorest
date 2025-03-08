2766cdb0614d15a14e1f255d282315d6d432947c
YogeshRajaram <yogiyogeshrajaram@gmail.com>
2025-03-08 11:46:43 +0530

    adding review model

--- watchmate/watchlist_app/api/serializer.py
+++ watchmate/watchlist_app/api/serializer.py
@@ -1,9 +1,14 @@
 from rest_framework import serializers
-from watchlist_app.models import WatchList,StreamPlatform
+from watchlist_app.models import WatchList,Review,StreamPlatform
 
+class ReviewSerializer(serializers.ModelSerializer):
+       
+       class Meta:
+              model = Review
+              fields = '__all__'
 
 class WatchListSerializer(serializers.ModelSerializer):
-    
+       reviews = ReviewSerializer(many=True,read_only=True)
        class Meta:
               model = WatchList
               fields = '__all__'
@@ -15,3 +20,4 @@ class StreamPlatformSerializer(serializers.ModelSerializer):
        class Meta:
               model = StreamPlatform
               fields = '__all__'
+
