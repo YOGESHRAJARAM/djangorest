db1cbb0550e433b2d28f72b1fabd691b97df9696
YogeshRajaram <yogiyogeshrajaram@gmail.com>
2025-03-08 10:58:58 +0530

    serializer relation

--- watchmate/watchlist_app/api/serializer.py
+++ watchmate/watchlist_app/api/serializer.py
@@ -9,6 +9,8 @@ class WatchListSerializer(serializers.ModelSerializer):
               fields = '__all__'
        
 class StreamPlatformSerializer(serializers.ModelSerializer):
+
+       watchlist = WatchListSerializer(many=True,read_only=True)
     
        class Meta:
               model = StreamPlatform
